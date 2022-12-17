9c308f7ccafb800876725978680e2e5966276b2e
uaravindshenoy <75222342+uaravindshenoy@users.noreply.github.com>
2022-12-17 09:02:21 +0530

    daily

---
+++ feast.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/5aa736a455f906981800360d/javascript
+
+function feast(beast, dish) {
+    return (beast.startsWith(dish[0])&&beast.endsWith(dish[dish.length-1]))
+   }
+  
+  
+  
