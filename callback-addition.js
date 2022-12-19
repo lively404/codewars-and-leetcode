7238915b08de06da3545bcdedec6b26861c9588c
uaravindshenoy <75222342+uaravindshenoy@users.noreply.github.com>
2022-12-19 09:00:38 +0530

    daily solution

---
+++ callback-addition.js
@@ -0,0 +1,49 @@
+  
+  // https://www.codewars.com/kata/525f3eda17c7cd9f9e000b39/train/javascript
+  
+  function zero() {
+    if(arguments.length) return  Math.floor(eval(0+[...arguments][0]));
+    else return 0;
+  }
+  function one() {
+      if(arguments.length) return  Math.floor(eval(1+[...arguments][0]));
+    else return 1;
+  }
+  function two() {
+      if(arguments.length) return  Math.floor(eval(2+[...arguments][0]));
+    else return 2;
+  }
+  function three() {
+      if(arguments.length) return  Math.floor(eval(3+[...arguments][0]));
+    else return 3;
+  }
+  function four() {
+      if(arguments.length) return  Math.floor(eval(4+[...arguments][0]));
+    else return 4;
+  }
+  function five() {
+      if(arguments.length) return  Math.floor(eval(5+[...arguments][0]));
+    else return 5;
+  }
+  function six() {
+      if(arguments.length) return  Math.floor(eval(6+[...arguments][0]));
+    else return 6;
+  }
+  function seven() {
+      if(arguments.length) return  Math.floor(eval(7+[...arguments][0]));
+    else return 7;
+  }
+  function eight() {
+    if(arguments.length) return  Math.floor(eval(8+[...arguments][0]));
+  else return 8;
+}
+function nine() {
+    if(arguments.length) return Math.floor(eval(9+[...arguments][0]));
+  else return 9;
+}
+
+function plus(num) {return "+"+num;}
+function minus(num) {return "-"+num;}
+function times(num) {return "*"+num;}
+function dividedBy(num) {return "/"+num;}
+
