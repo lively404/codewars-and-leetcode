4b6d46e3e00db8d12d91d6a4a97d4b0e5db09ecf
uaravindshenoy <75222342+uaravindshenoy@users.noreply.github.com>
2023-01-03 07:56:10 +0530

    daily puzzle solution

---
+++ powers-of-two.js
@@ -0,0 +1,11 @@
+     // https://www.codewars.com/kata/57a083a57cb1f31db7000028/train/javascript
+
+     function powersOfTwo(n){
+        let op = [];
+        for(let i = 0; i <= n; i++){
+          op.push(2**i)
+        }
+        return op;
+      }
+  
+  
