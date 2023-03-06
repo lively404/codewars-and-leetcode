dcffa80b7c85686ade905b6817aeac9deca86443
uaravindshenoy <75222342+uaravindshenoy@users.noreply.github.com>
2023-03-06 22:17:24 +0530

    Daily puzzle

---
+++ 230306.js
@@ -0,0 +1,11 @@
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
+
+/* function repeatStr (n, s) {
+  let string = ''
+  for(let i = 0; i < n; i++){
+    string+=s;
+  };
+  return string;
+} */
